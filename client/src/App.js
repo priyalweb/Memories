58638461befd84bde900cab9c27dbc68c3be1f63
Priyal Dubey <priyaldubey6863@gmail.com>
2021-08-03 14:45:03 +0530

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import React,{useEffect, useState} from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import memories from './images/memories.png';
+import useStyles from './styles';
+
+import {getPosts} from './actions/posts';
+
+const App = () => {
+  const[currentId, setCurrentId] = useState(null);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            Memories
+          </Typography>
+          <img className={classes.image} src={memories} alt="memories" height="60" />
+      </AppBar>
+      {/* grow provides simple animations*/}
+      <Grow in>
+        <Container>
+          <Grid container className={classes.mainContainer} justify="space-between" alignItems="strech" spacing={3} >
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId}/>              
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {Provider } from 'react-redux';
+import {createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+// const initialState = {};
+const middleware = [thunk];
+
+const store = createStore(
+  reducers,
+  // initialState,  
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
